7bd0c55cd11dc588bcf0d0e906b3bc3e5fbb2bd5
kubinpe5 <kubinpe5@fit.cvut.cz>
2016-01-23 18:03:25 +0100

    Save

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: "tickets#index"
+  root to: 'tickets#index'
   resources :tickets
+  get 'users' => 'users#index'
   # resources :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
